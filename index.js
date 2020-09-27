f66473096dbaa4cc64536c6cf78f3df4914321d1
David Pan <dpan5425@gmail.com>
2020-09-26 23:25:25 -0400

    added bio section

---
+++ index.js
@@ -0,0 +1,13 @@
+// Blinking cursor effect
+var cursor = true;
+var speed = 500;
+
+setInterval(() => {
+    if(cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+    }else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+    }
+}, speed);
