d4878d501a91e4108bc191a40cdc516a8a8804b8
David Pan <dpan5425@gmail.com>
2020-11-20 16:40:10 -0500

    svg opacity on scroll

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 $(window).scroll(function(){
     $(".bio-main").css("opacity", 1 - $(window).scrollTop() / 500);
-
+    $(".dp-header-background").css("opacity", 0 + $(window).scrollTop() / 900);
   });
