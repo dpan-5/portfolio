f5352e76a8e5a53cf190be1fe0c62180c0d251ff
David Pan <dpan5425@gmail.com>
2020-11-20 18:20:08 -0500

    added some projects

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(window).scrollTop(0);
 
 $(window).scroll(function(){
-    $(".bio-main").css("opacity", 1 - $(window).scrollTop() / 500);
+    $(".bio-main").css("opacity", 1.3 - $(window).scrollTop() / 400);
 
-    $(".dp-header-background").css("opacity", -1.2 + $(window).scrollTop() / 400);
+    $(".dp-header-background").css("opacity", -1.2 + $(window).scrollTop() / 300);
+
+    // $(".projects-main").css("opacity", -1.2 + $(window).scrollTop() / 300);
     
   });
