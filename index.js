c353c49cc72d25f63f242c0b259dd214d98a5635
David Pan <dpan5425@gmail.com>
2020-12-13 21:45:46 -0500

    Modify text

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
+$(window).scroll(function () {
+  $(".bio-main").css("opacity", 1.3 - $(window).scrollTop() / 400);
 
-  // $(window).scrollTop(0);
-  $(".dp-header-background").css("opacity", 0);
-  console.log("updated");
+  $(".dp-header-background").css("opacity", -1.2 + $(window).scrollTop() / 300);
 
-  $(window).scroll(function(){
-    
-
-    $(".bio-main").css("opacity", 1.3 - $(window).scrollTop() / 400);
-
-    $(".dp-header-background").css("opacity", -1.2 + $(window).scrollTop() / 300);
-
-    // $(".projects-main").css("opacity", -1.2 + $(window).scrollTop() / 300);
-    // if ($(window).scrollTop() > 900) {
-    //   $("svg").css("visibility", "visibile");
-    // }
-    
-  });
-
-
-
+  // $(".projects-main").css("opacity", -1.2 + $(window).scrollTop() / 300);
+  // if ($(window).scrollTop() > 900) {
+  //   $("svg").css("visibility", "visibile");
+  // }
 });
+
+// Blinking cursor effect
+let cursor = true;
+
+setInterval(() => {
+  if (cursor) {
+    document.getElementById("cursor").style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById("cursor").style.opacity = 1;
+    cursor = true;
+  }
+}, 500);
